248f28a8b3ba71f14468266155890704327063b2
Sarvesh Patil <sarvesh.patil@plivo.com>
2026-07-17 13:35:42 +0530

    Add src/main/java/io/kestra/plugin/plivo/call/package-info.java

---
+++ src/main/java/io/kestra/plugin/plivo/call/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "Tasks that place outbound voice calls via the Plivo Voice API.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.plivo.call;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
