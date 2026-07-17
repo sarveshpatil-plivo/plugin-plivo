b124175e0814b036897c50553a320bdc98910dd9
Sarvesh Patil <sarvesh.patil@plivo.com>
2026-07-17 13:35:48 +0530

    Add src/main/java/io/kestra/plugin/plivo/message/package-info.java

---
+++ src/main/java/io/kestra/plugin/plivo/message/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "Tasks and triggers that send and receive SMS messages via the Plivo Messages API.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.plivo.message;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
